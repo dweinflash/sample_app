ad56324fc6d8f6a8f74a871e05655fd8f730f9ff
David Weinflash <david.weinflash@gmail.com>
2020-09-22 21:05:21 +0000

    Add projects to home page and started Cozmo project page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
+  get  '/cozmo',   to: 'static_pages#cozmo'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
