ab0d7badf43d34645d267e8f6a5d218378552a9e
David Weinflash <david.weinflash@gmail.com>
2023-09-10 00:52:50 +0000

    Add Audio Search

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get  '/help',      to: 'static_pages#help'
   get  '/about',     to: 'static_pages#about'
+  get  '/audio',     to: 'static_pages#audiosearch'
   get  '/cozmo',     to: 'static_pages#cozmo'
   get  '/sat',       to: 'static_pages#sat'
   get  '/android',   to: 'static_pages#android'
